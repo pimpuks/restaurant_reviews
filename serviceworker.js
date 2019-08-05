a0195412a549ea099fdd015ca10dfdb23901ceec
Sansuth <pimpuks@6c96cfdf6731.ant.amazon.com>
2019-08-05 23:14:25 +0800

    debug exception during install service worker

--- serviceworker.js
+++ serviceworker.js
@@ -22,13 +22,13 @@ self.addEventListener('install', event => {
     './img/7.jpg',
     './img/8.jpg',
     './img/9.jpg',
-    './img/10.jpg',
-    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-    'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw',
-    'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw'
+    './img/10.jpg'
+    // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
+    // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+    // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw',
+    // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw'
   ];
   event.waitUntil(
     caches.open(STATIC_CACHE).then(cache => {
