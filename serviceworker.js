835bc6bcb67aa79a241372ca7cad1a30fc332d46
Sansuth <pimpuks@6c96cfdf6731.ant.amazon.com>
2019-08-06 08:27:36 +0800

    debug messsage

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,6 @@ const STATIC_CACHE = `restaurant-reviews-cache-v${CACHE_VERSION}`;
 
 self.addEventListener('install', event => {
   let urlsToCache = [
-    // './',
     './index.html',
     './restaurant.html',
     './serviceworker.js',
@@ -12,7 +11,6 @@ self.addEventListener('install', event => {
     './js/dbhelper.js',
     './data/restaurants.json',
     './css/styles.css',
-    // './img/',
     './img/1.jpg',
     './img/2.jpg',
     './img/3.jpg',
@@ -22,13 +20,13 @@ self.addEventListener('install', event => {
     './img/7.jpg',
     './img/8.jpg',
     './img/9.jpg',
-    './img/10.jpg'
-    // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-    // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-    // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-    // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw',
-    // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw'
+    './img/10.jpg',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
+    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+    'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw',
+    'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoicGltcHVrcyIsImEiOiJjanltajR1cncwanF3M2lzOHJlbW54cTF3In0.BEqfumdmKy4IpCn1sA-xHw'
   ];
   event.waitUntil(
     caches.open(STATIC_CACHE).then(cache => {
@@ -38,6 +36,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  console.log(event.request);
   event.respondWith(
     caches.match(event.request).then(resp => {
       return (
